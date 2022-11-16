81ca4d690e1a859edf98010749e212bd47847134
mclewis8 <mclewis8@crimson.ua.edu>
2022-11-16 16:53:30 -0600

    created defs for SET and FIND in env class

--- impls/cpp/env.hpp
+++ impls/cpp/env.hpp
@@ -14,7 +14,30 @@ public:
     Env(Env *outer)
         : m_outer(outer) { }
 
+    // defining three methods for the env object
+
+    // SET: takes a symbol key and a mal value and adds to the data structure
+    void set(SymbolValue *key, Value *val) {
+        m_data[key] = val;
+    }
+
+    // FIND: takes a symbol key and if the current environment contains
+    // that key then return the environment. If no key if found
+    // and outer is not nil then call find() (recursive) on the outer environment.
+    Env *find(const SymbolValue *key) const {
+        auto search = m_data.find(key);
+        if (search != m_data.end())
+            return const_cast<Env *>(this);
+        else if (m_outer)
+            return m_outer->find(key);
+        return nullptr;
+    }
+
+    // GET: takes a symbol key and uses the find() method to locate the environment
+    // with the key, then returns the mathing value. If no key is found
+    // up the outer chain, then throws a "not found" error.
+
 private:
     Env *m_outer { nullptr };
-    unordered_map<SymbolValue *, Value *, EnvHash, EnvComparator> m_data;
+    unordered_map<const SymbolValue *, Value *, EnvHash, EnvComparator> m_data;
 };
