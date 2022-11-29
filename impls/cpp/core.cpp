4cedb3e52c7282de9598b55b895cb307ab806db6
mclewis8 <mclewis8@crimson.ua.edu>
2022-11-29 15:13:16 -0600

    added new tests in step4.mal

--- impls/cpp/core.cpp
+++ impls/cpp/core.cpp
@@ -134,13 +134,14 @@ Value *cnt(size_t argc, Value **args) {
 }
 
 Value *eq(size_t argc, Value **args) {
+    
     assert(argc >= 2);
     auto a = args[0];
     auto b = args[1];
 
     if(*a == b) 
         return TrueValue::the();
-    return FalseValue::the();
+    return NilValue::the();
 
 }
 
